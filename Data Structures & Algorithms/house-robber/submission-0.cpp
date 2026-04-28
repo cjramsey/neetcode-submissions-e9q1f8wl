712b6eaab14548a661b7a9f9f9fbfb9eb4b60968
Cameron Ramsey <cameronramsey04@gmail.com>
2026-04-28 09:00:51 +0100

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() <= 2) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        nums[2] += nums[0]; 
+
+        for (int i = 3; i < nums.size(); ++i) {
+            nums[i] += max(nums[i - 2], nums[i - 3]);
+        }
+
+        return max(nums[nums.size() - 1], nums[nums.size() - 2]);
+    }
+};
