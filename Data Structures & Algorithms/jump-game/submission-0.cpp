a40dbe649aecbf3f99105c3a56d5809266ebf1b3
Cameron Ramsey <cameronramsey04@gmail.com>
2026-05-10 08:16:14 +0100

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return true;
+        }
+        vector<bool> dp(n, false);
+        dp[n - 1] = true;
+
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 1; j <= nums[i]; ++j) {
+                if (dp[i + j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
